4655d4cd30b958b1b67ccafccbd2d4db3d1b9297
贺敏 <hemin_it@163.com>
2016-09-05 20:30:37 +0800

    first commit

---
+++ app/src/main/java/com/minbo/myhelloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.minbo.myhelloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "贺敏何敏9999999", Toast.LENGTH_SHORT).show();
+    }
+}
