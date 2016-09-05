6612a30e914b2b4a8b8bf02b2ca7eb1f591fc22c
Minbo.He <hemin_it@163.com>
2016-09-05 23:30:38 +0800

    second commit

--- app/src/main/java/com/minbo/myhelloworld/MainActivity.java
+++ app/src/main/java/com/minbo/myhelloworld/MainActivity.java
@@ -1,16 +1,41 @@
 package com.minbo.myhelloworld;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import javax.xml.datatype.Duration;
+
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "贺敏何敏9999999", Toast.LENGTH_SHORT).show();
+
+        t = (TextView) findViewById(R.id.textView2);
+        t.setText("hemin123");
+        t.setBackgroundColor(Color.GREEN);
+
+        Button b = (Button) findViewById(R.id.button);
+        ButtonListener listener = new ButtonListener();
+        b.setOnClickListener(listener);
+    }
+
+    class ButtonListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            count++;
+            t.setText(count + "");
+            Toast.makeText(MainActivity.this, "count=" + count, Toast.LENGTH_SHORT).show();
+        }
     }
 }
