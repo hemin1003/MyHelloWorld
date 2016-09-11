83a64cb53b21af7f36443bd9b78d1d340778f9b4
Minbo.He <hemin_it@163.com>
2016-09-11 22:38:18 +0800

    study dialog

---
+++ app/src/main/java/com/minbo/myhelloworld/widget/TestDailog.java
@@ -0,0 +1,13 @@
+package com.minbo.myhelloworld.widget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestDailog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_dailog);
+    }
+}
---
+++ app/src/main/java/com/minbo/myhelloworld/widget/ToastUtils.java
@@ -0,0 +1,7 @@
+package com.minbo.myhelloworld.widget;
+
+/**
+ * Created by Minbo on 9/11/16.
+ */
+public class ToastUtils {
+}
